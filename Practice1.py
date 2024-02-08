10a6527ab9f64f5355fb720bf1476d7cef5a2a79
yehor-papkovych <yehor.papkovych@auk.edu.ua>
2024-02-08 15:20:57 +0200

    Initial commit

---
+++ Practice1.py
@@ -0,0 +1,32 @@
+# Part 1
+print("Hello!")
+print("This is my first Python program!")
+
+# Part 2
+x = 10
+print(x)
+
+y = 15
+print(y)
+
+z=0.7
+print(z)
+
+course_name = "Introduction To Programming Logic"
+print(course_name)
+
+a = True
+b = False
+
+# Part 3
+sum = x + y
+print(sum)
+
+print(x > y)
+print(x != y)
+
+print(a and b)
+print(a or b)
+
+na = not a
+print(na)
